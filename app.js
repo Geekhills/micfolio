e6e1ea24ce3c36cca37323f6a5b2b567c0e5d1ba
Michael Ofori Atta <oforiatta.michael@live.com>
2021-10-01 02:12:06 +0000

    prismic implementation

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  });
+}
+
+function handleLinkResolver(doc) {
+  
+  // Define the url depending on the document type
+  // if (doc.type === 'page') {
+  //   return '/page/' + doc.uid;
+  // } else if (doc.type === 'blog_post') {
+  //   return '/blog/' + doc.uid;
+  // }
+  
+  // Default to homepage
+  return '/';
+}
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+  };
+  res.locals.PrismicDOM = PrismicDOM;
+  next();
+});
 
 const path = require('path')
 const port = 3000
@@ -8,11 +43,35 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('pages/home')
-})
+  initApi(req).then(api => {
+    api.query([
+      Prismic.Predicates.any('document.type', ['meta','home']),
+      ]).then(response => {
+      const { results } = response
+     const [meta,home] = results
+     console.log(home.data.title)
+      res.render('pages/home', {
+        meta,home
+        
+        });
+    });
+  });})
 
-app.get('/about', (req, res) => {
-    res.render('pages/about')
+app.get('/about', async (req, res) => {
+
+    initApi(req).then(api => {
+      api.query([
+        Prismic.Predicates.any('document.type', ['meta','about']),
+        ]).then(response => {
+        const { results } = response
+       const [meta,about] = results
+       console.log(meta,about)
+        res.render('pages/about', {
+          meta,about
+          
+          });
+      });
+    });
 })
 
 
